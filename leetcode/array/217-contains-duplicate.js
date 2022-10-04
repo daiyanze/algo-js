11258cac46028adfdc0fcc468c6b9374a87d5003
Yanze Dai <ginoalex8964@yahoo.com>
2022-10-04 14:04:59 +0900

    refactor: remove leetcode folder

--- leetcode/array/217-contains-duplicate.js
+++
@@ -1,8 +0,0 @@
-function containDuplicate(nums) {
-  if (nums.length == 1) return false
-
-  return new Set(nums).size != nums.length
-}
-
-console.log(containDuplicate([1,1,2,3,4]))
-console.log(containDuplicate([1,2,3,4]))
--- leetcode/bit-manipulation/231-power-of-two.js
+++
@@ -1,9 +0,0 @@
-function isPowerOfTwo(n) {
-  if (n <= 0) return false
-
-  return !(n & (n - 1))
-}
-
-console.log(isPowerOfTwo(16))
-console.log(isPowerOfTwo(15))
-console.log(isPowerOfTwo(1))
